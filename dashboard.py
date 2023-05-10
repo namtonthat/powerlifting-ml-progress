117fa6d2c9ff785982e9912fdf02cd248042c071
Nam Tonthat <n.nam.tonthat@gmail.com>
2023-05-10 21:54:06 +1000

    🔥 chore(dashboard.py): remove unused imports and download code 📦 chore(pyproject.toml): add streamlit to dependencies

--- dashboard.py
+++ dashboard.py
@@ -1,8 +1,1 @@
-import polars as pl
-import zipfile
-
-
-POWERLIFTING_DATA_URL = "https://openpowerlifting.gitlab.io/opl-csv/files/openipf-latest.zip"
-
-with zipfile.ZipFile("file.zip", "r") as zip_ref:
-    zip_ref.extractall(".")
+import streamlit as st
