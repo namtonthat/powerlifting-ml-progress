94ca42291b1c58fb5f861f0ca03b58a2b78a2568
Nam Tonthat <n.nam.tonthat@gmail.com>
2023-05-13 09:49:38 +1000

    first working version of machine-learning page

--- about.py
+++ about.py
@@ -3,13 +3,11 @@ from pathlib import Path
 
 st.set_page_config(
     page_title="About",
-    page_icon="👋",
+    page_icon="🏋️‍♂️",
 )
 
 st.write("# Welcome to `powerlifting-ml-progress`! 👋")
 
 readme_markdown = Path("README.md").read_text()
 
-st.markdown(
-    readme_markdown
-)
+st.markdown(readme_markdown)
