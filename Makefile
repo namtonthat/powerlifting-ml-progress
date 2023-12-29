7b9957822f940ee092ce5c3846d796d9a72c0c3d
Nam Tonthat <n.nam.tonthat@gmail.com>
2023-12-29 13:20:36 +1100

    update project.env and fix poetry / pyenv shell issue

---
+++ Makefile
@@ -0,0 +1,10 @@
+default: help
+
+.PHONY: help
+help: # 🆘 Show help for each of the Makefile recipes.
+	@grep -E '^[a-zA-Z0-9 -]+:.*#'  Makefile | sort | while read -r l; do printf "\033[1;32m$$(echo $$l | cut -f 1 -d':')\033[00m:$$(echo $$l | cut -f 2- -d'#')\n"; done
+
+.PHONY: setup
+setup: # 🔧 Install packages required for local development
+	@echo "Installing packages required for local development"
+	@./scripts/setup.sh $(arch)
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,55 +3,20 @@
 #   This script is used to setup the environment locally for this repository.
 ##
 
-# Detecting OS
-OS_NAME=$(uname)
-RELEASE_INFO=$(lsb_release -si 2>/dev/null || echo "Not Ubuntu")
-
 source .env/project.env
 
 echo "🔄 Removing existing virtual environment..."
-pyenv virtualenv-delete -f ${REPO_NAME}-${PYTHON_VERSION}
+venv_name=${REPO_NAME}-${PYTHON_VERSION}
+pyenv virtualenv-delete -f "$venv_name"
 
-if [[ "$OS_NAME" == "Darwin" ]]; then
-	echo "⚙️  Creating arch prefix for mac OS"
-	if [[ "$arch" == "x86_64" ]]; then
-		ARCH_PREFIX="arch -x86_64"
-	elif [[ "$arch" == "arm64" ]]; then
-		ARCH_PREFIX="arch -arm64"
-	else
-		echo "Unsupported arch, defaults to x86_64, can also be arm64"
-	fi
-	# MacOS
-	if [[ "$reinstall_python" == "true" ]]; then
-		echo "⬇️  Installing python: ${PYTHON_VERSION}"
-		$ARCH_PREFIX pyenv install ${PYTHON_VERSION}
-	else
-		echo "🔧  Using existing python: ${PYTHON_VERSION}"
-	fi
-
-	echo "🔧  Installing awscli..."
-	$ARCH_PREFIX brew install awscli
-
-elif [[ "$RELEASE_INFO" == "Ubuntu" ]]; then
-	# Ubuntu 22.04
-	echo "⬇️  Installing python: ${PYTHON_VERSION}"
-	sudo apt update
-	sudo apt install -y python${PYTHON_VERSION}
-
-	echo "🔧  Installing awscli..."
-	sudo apt install -y awscli
-
-else
-	echo "Unsupported OS or version."
-fi
-
-echo "🌍  Virtual environment setup"
-pyenv virtualenv ${PYTHON_VERSION} $REPO_NAME-${PYTHON_VERSION}
-pyenv local ${REPO_NAME}-${PYTHON_VERSION}
+echo "🌍 Virtual environment setup"
+pyenv virtualenv "${PYTHON_VERSION}" "$venv_name"
+pyenv local "$venv_name"
 pip install --upgrade pip setuptools wheel poetry
 
 echo "📦 Install dependencies via poetry..."
-poetry install
+poetry shell # this is needed to make sure that poetry respects the pyenv virtualenv
+poetry install --no-root
 
 echo "🔧 Install pre-commit hooks..."
 pre-commit clean
