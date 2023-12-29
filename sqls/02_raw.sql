dadadfc7416cc7483af086371a3f235e2ccbdeb4
Nam Tonthat <n.nam.tonthat@gmail.com>
2023-12-29 15:32:47 +1100

    add raw script

--- sqls/02_raw.sql
+++ sqls/02_raw.sql
@@ -1,7 +1,7 @@
 CREATE TABLE raw.natural_powerlifting AS (
-  SELECT * FROM landing.powerlifting
-  WHERE
-    [event] = 'SBD'
-    AND tested = 'Yes'
-    AND equipment = 'Raw'
-);
+    SELECT * FROM landing.powerlifting
+    WHERE
+        [event] = 'SBD'
+        AND tested = 'Yes'
+        AND equipment = 'Raw'
+)
